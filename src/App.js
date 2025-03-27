468fa188a9963550a797a6b572bb498dc980afb4
Prateek_Bahad <prateekbahad@gmail.com>
2025-03-27 14:19:49 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import About from "./components/About";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import TextForm from "./components/TextForm";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light")
+
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar mode={mode} title="TextCounter"/>
         <div className="container">
           <Routes>
             <Route path="/about" element=<About /> />
-            <Route path="/" element=<TextForm /> />
+            <Route path="/" element=<TextForm heading="TextCounter | WordCounter Character Counter" /> />
           </Routes>
         </div>
       </Router>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const TextForm = () => {
+const TextForm = (props) => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <div className="mb-3 my-5">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          <h2>{props.heading}</h2>
+        </label>
+        <textarea
+          className="form-control my-2"
+          id="exampleFormControlTextarea1"
+          rows="5"
+        ></textarea>
+      </div>
+      <div>
+        <button type="button" className="btn btn-primary mx-1 my-1">
+          UpperCase
+        </button>
+        <button type="button" className="btn btn-primary mx-1 my-1">
+          LowerCase
+        </button>
+        <button type="button" className="btn btn-primary mx-1 my-1">
+          TextCopy
+        </button>
+        <button type="button" className="btn btn-primary mx-1 my-1">
+          TextClear
+        </button>
+      </div>
+      <div>
+        <h2 className="pt-3">Summary</h2>
+        <p>0 words 0 characters</p>
+        <p>0 minute for reading</p>
+      </div>
+      <div>
+        <h2 className="pt-2">Text Preview</h2>
+        <p>No Text Preview!</p>
+      </div>
+    </>
+  );
+};
 
-export default TextForm
+export default TextForm;
