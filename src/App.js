9d577fd15e9b4b45f684fb1452109fcb4f4ae352
Prateek_Bahad <prateekbahad@gmail.com>
2025-03-27 13:10:05 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import './index.css';
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <div className="container">
-        <TextForm/>
-        <About/>
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/about" element=<About /> />
+            <Route path="/" element=<TextForm /> />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
