bcf734f7f61927bd446a9fa4b6a02de2ac67e1d0
Prateek_Bahad <prateekbahad@gmail.com>
2025-03-27 14:25:18 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import TextForm from "./components/TextForm";
@@ -6,15 +6,15 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("light")
+  // const [mode, setMode] = useState("light")
 
   return (
     <>
       <Router>
-        <Navbar mode={mode} title="TextCounter"/>
+        <Navbar title="TextCounter"/>
         <div className="container">
           <Routes>
-            <Route path="/about" element=<About /> />
+            <Route path="/about" element=<About heading="About Us" /> />
             <Route path="/" element=<TextForm heading="TextCounter | WordCounter Character Counter" /> />
           </Routes>
         </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const About = () => {
+const About = (props) => {
   return (
     <>
      <div className="container">
-        <h1 className='mt-5'>About Us</h1>
-        <h4 className='mt-5'><strong>Text Analyzer</strong></h4>
+        <h2 className='mt-5'>{props.heading}</h2>
+        <h5 className='mt-5'><strong>Text Analyzer</strong></h5>
         <p>To check word count, simply place your cursor into the text box above and start typing. You'll see the number of characters and words increase or decrease as you type, delete, and edit them. You can also copy and paste text from another program over into the online editor above.</p>
 
-        <h4 className='mt-5'><strong>Free to Use</strong></h4>
+        <h5 className='mt-5'><strong>Free to Use</strong></h5>
         <p>Knowing the word count of a text can be important. For example, if an author has to write a minimum or maximum amount of words for an article, essay, report, story, book, paper, you name it. WordCounter will help to make sure its word count reaches a specific requirement or stays within a certain limit.</p>
 
-        <h4 className='mt-5'><strong>Browser Compatibility</strong></h4>
+        <h5 className='mt-5'><strong>Browser Compatibility</strong></h5>
         <p>This Application is easy Compatable to Browsering Software like Firefox, Safari, Opera, Internet Explorer</p>
      </div> 
     </>
